62c70c47520e2718e2d04d140adf2bf0c4dffd61
OLAEMJO8 <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-01 20:15:46 -0300

    proyectos

--- src/Porfolio/Proyectos.jsx
+++ src/Porfolio/Proyectos.jsx
@@ -29,14 +29,18 @@ export default function Proyectos() {
           Proyectos
         </Heading>
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={8}>
-
-
-        <Link
+          <Link
             as="a"
-            href="https://github.com/OLAEMJO8/Profinder"
+            href="https://github.com/OLAEMJO8/Portafolio"
             target="_blank"
           >
-            <Box boxShadow={"2xl"} rounded={"md"} p={6} overflow={"hidden"}>
+            <Box
+              boxShadow={"2xl"}
+              rounded={"md"}
+              p={6}
+              overflow={"hidden"}
+              h={{ base: "300px", md: "700px" }}
+            >
               <Box
                 h={{ base: "150px", md: "200px" }}
                 mt={-6}
@@ -47,7 +51,7 @@ export default function Proyectos() {
                 <Image
                   bg={"gray.900"}
                   src={
-                    "https://user-images.githubusercontent.com/110426875/257066093-37c1034b-122a-4613-abf0-d5a70de7e2c9.png"
+                    "https://github.com/OLAEMJO8/Portafolio/raw/main/image.png"
                   }
                   alt="Example"
                   w="100%"
@@ -73,7 +77,13 @@ export default function Proyectos() {
                   Portafolio personal
                 </Heading>
                 <Text color={"white"} fontSize={{ base: "sm", md: "md" }}>
-                Este portafolio cuenta un poco de mi, mis skills, mis proyectos y podras ver un poco de mi ultimo trabajo actualmente.
+                  Este portafolio cuenta un poco de mi, mis skills, mis
+                  proyectos y podras ver un poco de mi ultimo trabajo
+                  actualmente.
+                  <br />
+                  Si estás buscando una desarrolladora apasionada, proactiva y
+                  con ganas de seguir aprendiendo, ¡no dudes en conectarte
+                  conmigo!
                 </Text>
               </Stack>
             </Box>
@@ -83,7 +93,13 @@ export default function Proyectos() {
             href="https://github.com/OLAEMJO8/Profinder"
             target="_blank"
           >
-            <Box boxShadow={"2xl"} rounded={"md"} p={6} overflow={"hidden"}>
+            <Box
+              boxShadow={"2xl"}
+              rounded={"md"}
+              p={6}
+              overflow={"hidden"}
+              h={{ base: "300px", md: "700px" }}
+            >
               <Box
                 h={{ base: "150px", md: "200px" }}
                 mt={-6}
@@ -123,9 +139,11 @@ export default function Proyectos() {
                   Profinder es una plataforma innovadora que permite a los
                   usuarios prestadores de servicios publicar sus servicios y
                   ofrecerlos dentro de diversas categorías en diferentes zonas
-                  de Latinoamérica. Los usuarios clientes pueden consultar estas
-                  publicaciones según su ubicación y establecer contacto directo
-                  con los prestadores de servicios para contratarlos.
+                  de Latinoamérica.
+                  <br />
+                  Los usuarios clientes pueden consultar estas publicaciones
+                  según su ubicación y establecer contacto directo con los
+                  prestadores de servicios para contratarlos.
                 </Text>
               </Stack>
             </Box>
@@ -136,7 +154,13 @@ export default function Proyectos() {
             href="https://github.com/OLAEMJO8/drivers"
             target="_blank"
           >
-            <Box boxShadow={"2xl"} rounded={"md"} p={6} overflow={"hidden"}>
+            <Box
+              boxShadow={"2xl"}
+              rounded={"md"}
+              p={6}
+              overflow={"hidden"}
+              h={{ base: "300px", md: "700px" }}
+            >
               <Box
                 h={{ base: "150px", md: "200px" }}
                 bg={"gray.100"}
@@ -175,8 +199,10 @@ export default function Proyectos() {
                   Visualización de cards de corredores, con la posibilidad de
                   filtrar, ordenarlos, buscar uno en específico a través de la
                   barra de búsqueda y se podrá crear un nuevo corredor a través
-                  de un formulario. Creada usando JavaScript, CSS, React, Redux,
-                  PostgreSQL, Express, Sequelize.
+                  de un formulario.
+                  <br />
+                  Creada usando JavaScript, CSS, React, Redux, PostgreSQL,
+                  Express, Sequelize.
                 </Text>
               </Stack>
             </Box>
