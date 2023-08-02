589d9f3a147773f853a49f4b526c54f6e4e062f5
OLAEMJO8 <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-02 12:53:50 -0300

    font

--- src/Porfolio/Card.jsx
+++ src/Porfolio/Card.jsx
@@ -1,42 +1,23 @@
-import {
-  Avatar,
-  Badge,
-  Box,
-  Button,
-  Center,
-  Container,
-  Divider,
-  Flex,
-  Heading,
-  Image,
-  Link,
-  Stack,
-  Text,
-  VStack,
-  Wrap,
-  WrapItem,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Center, Stack, Text } from "@chakra-ui/react";
 
 export default function Card() {
   return (
     <Center>
-      <Box w="100%"  maxW="2000px">
-        <VStack>
-          <Text
-            textTransform="uppercase"
-            fontSize={{ base: "7xl", md: "9xl" }}
-            color={"white"}
-            justifyContent="center"
-            textAlign="match-parent"
-            fontWeight="bold"
-            fontFamily="Lato, sans-serif"
-          >
-            Full Stack Developer Maria Jose Olaechea ReactJS ReduxJS Tailwind
-            CSS Node.js 
-          </Text>
-        </VStack>
-      </Box>
+      <Stack w="100%">
+        <Text
+          textTransform="uppercase"
+          fontSize={{ base: "7xl", md: "10.5rem" }}
+          color={"white"}
+          justifyContent="center"
+          textAlign="left"
+          fontWeight="bold"
+          fontFamily="Bebas Neue, sans-serif"
+          // style={{ fontSize: "10.5rem" }}
+        >
+          Full Stack Developer Maria Jose Olaechea ReactJS ReduxJS Tailwind CSS
+          Node.js
+        </Text>
+      </Stack>
     </Center>
   );
 }
