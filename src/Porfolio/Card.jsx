466b8027a5c52fb071b7eeb37d5dae6e23867546
OLAEMJO8 <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-02 21:42:28 -0300

    9

--- src/Porfolio/Card.jsx
+++ src/Porfolio/Card.jsx
@@ -7,7 +7,7 @@ export default function Card() {
         <Text
           textTransform="uppercase"
           // fontSize={{ base: "md", md: "3.75rem" }}
-          fontSize={{ base: "7xl", md: "10rem" }}
+          fontSize={{ base: "7xl", md: "9rem" }}
           color={"white"}
           justifyContent="center"
           textAlign="left"
