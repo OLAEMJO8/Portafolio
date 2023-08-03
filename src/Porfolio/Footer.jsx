2b33e639488e89e00c8f466a14926c5dd2936f61
OLAEMJO8 <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-03 02:00:56 -0300

    1

--- src/Porfolio/Footer.jsx
+++ src/Porfolio/Footer.jsx
@@ -4,7 +4,7 @@ import { Stack, Box } from "@chakra-ui/react";
 function Footer() {
   return (
 
-    <Stack color={"white"} alignItems={"center"}  justifyContent="center" w="100%" height="50vh" spacing={8}>
+    <Stack color={"white"} alignItems={"center"}  justifyContent="center" w="100%" height="80vh" spacing={8}>
       <Box alignItems={"center"} justifyContent={"center"}>
         {" "}
         Maria Jose Olaechea - 2023 - Todos los derechos reservados
