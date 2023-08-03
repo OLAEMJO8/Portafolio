e80d55091ebc84f5e3c50f566644c96e6131325a
OLAEMJO8 <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-03 01:51:39 -0300

    1

--- src/Porfolio/Footer.jsx
+++ src/Porfolio/Footer.jsx
@@ -3,7 +3,7 @@ import { HStack, Box } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <HStack color={"white"} justify="center" w="100%" height="20vh">
+    <HStack color={"white"} justify="center" w="100%" height="50vh">
       <Box alignItems={"center"} justifyContent={"center"}>
         {" "}
         Maria Jose Olaechea - 2023 - Todos los derechos reservados
