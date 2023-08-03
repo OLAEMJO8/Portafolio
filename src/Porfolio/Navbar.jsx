016452dda3ed985aa0f3b2608500a68d65c7b666
Maria Jose Olaechea <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-02 23:19:04 -0300

    Update Navbar.jsx

--- src/Porfolio/Navbar.jsx
+++ src/Porfolio/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Box, Flex, Button, Stack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Box, Flex, Button, Stack, IconButton, useDisclosure } from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = ({
   scrollToContact,
@@ -8,10 +7,12 @@ const Navbar = ({
   scrollToSoftSkills,
   scrollToProyectos,
 }) => {
+  const mobileNav = useDisclosure();
+
   return (
     <>
       <Box px={4}>
-        <Flex h={16} alignItems="center" justifyContent="center">
+        <Flex h={16} alignItems="center" justifyContent="space-between" px={4}>
           <Stack direction="row" spacing={3}>
             <Button
               onClick={scrollToContact}
@@ -46,7 +47,49 @@ const Navbar = ({
               Proyectos
             </Button>
           </Stack>
+
+          <Box display={{ base: "inline-flex", md: "none" }}>
+            <IconButton
+              aria-label="Open menu"
+              fontSize="20px"
+              color="white"
+              _hover={{ bg: "teal.500" }}
+              icon={<AiOutlineMenu />}
+              onClick={mobileNav.onOpen}
+            />
+          </Box>
         </Flex>
+
+        <Box display={{ base: "inline-flex", md: "none" }}>
+          <VStack
+            pos="absolute"
+            top={0}
+            left={0}
+            right={0}
+            display={mobileNav.isOpen ? "flex" : "none"}
+            flexDirection="column"
+            p={2}
+            pb={4}
+            m={2}
+            bg="teal.500"
+            spacing={3}
+            rounded="sm"
+            shadow="sm"
+          >
+            <Button variant="ghost" color="white" onClick={scrollToContact}>
+              Contacto
+            </Button>
+            <Button variant="ghost" color="white" onClick={scrollToTechSkills}>
+              Tech Skills
+            </Button>
+            <Button variant="ghost" color="white" onClick={scrollToSoftSkills}>
+              Soft Skills
+            </Button>
+            <Button variant="ghost" color="white" onClick={scrollToProyectos}>
+              Proyectos
+            </Button>
+          </VStack>
+        </Box>
       </Box>
     </>
   );
