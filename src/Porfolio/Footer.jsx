6811acd0f7ef84297494cb74fa7657c4404076a7
OLAEMJO8 <118780477+OLAEMJO8@users.noreply.github.com>
2023-08-03 01:56:26 -0300

    1

--- src/Porfolio/Footer.jsx
+++ src/Porfolio/Footer.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { HStack, Box } from "@chakra-ui/react";
+import { Stack, Box } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <HStack color={"white"} justify="center" w="100%" height="50vh">
+
+    <Stack color={"white"} alignItems={"center"}  justifyContent="center" w="100%" height="50vh" spacing={8}>
       <Box alignItems={"center"} justifyContent={"center"}>
         {" "}
         Maria Jose Olaechea - 2023 - Todos los derechos reservados
       </Box>
-    </HStack>
+    </Stack>
   );
 }
 
